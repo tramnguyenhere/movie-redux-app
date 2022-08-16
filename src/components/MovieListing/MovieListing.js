af6d91dce4ad5571240901a53c0984cef4356dda
Tram Nguyen <tramngoc1402@gmail.com>
2022-08-16 21:25:57 +0300

    Building movie carousel

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -3,6 +3,8 @@ import { useSelector } from 'react-redux';
 import { getAllMovies, getAllShows } from '../../features/movies/movieSlice';
 import MovieCard from '../MovieCard/MovieCard';
 import '../MovieListing/MovieListing.scss';
+import Slider from 'react-slick';
+import { Settings } from '../../common/settings';
 
 const MovieListing = () => {
   const movies = useSelector(getAllMovies);
@@ -36,11 +38,15 @@ const MovieListing = () => {
     <div className='movie-wrapper'>
       <div className='movie-list'>
         <h2>Movies</h2>
-        <div className='movie-container'>{renderMovies}</div>
+        <div className='movie-container'>
+          <Slider {...Settings}>{renderMovies}</Slider>
+        </div>
       </div>
       <div className='show-list'>
         <h2>Shows</h2>
-        <div className='show-container'>{renderShows}</div>
+        <div className='show-container'>
+          <Slider {...Settings}>{renderShows}</Slider>
+        </div>
       </div>
     </div>
   );
